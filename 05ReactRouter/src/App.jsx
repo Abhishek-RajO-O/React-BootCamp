ffd9832f43fe0e14ac59f664fb2ef947decb2ae0
Abhishek Raj <abhishekraj605912@gmail.com>
2026-07-12 00:17:22 +0530

    Added Structure

---
+++ 05ReactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+   <>
+   {/* <h1 className='bg-'>hdshw</h1>
+    */}
+    <h1 class="text-3xl font-bold underline bg-green-500">
+    Hello world!
+  </h1>
+   </>
+  )
+}
+
+export default App
